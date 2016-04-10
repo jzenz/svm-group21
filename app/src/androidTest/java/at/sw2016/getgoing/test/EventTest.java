86ecd0f22213618f9894e5764145c09d74420dee
Michael Schiller <michael.schiller@student.tugraz.at>
2016-04-10 18:58:50 +0200

    [MS,LS] added testcases for and Event class

---
+++ app/src/androidTest/java/at/sw2016/getgoing/test/EventTest.java
@@ -0,0 +1,62 @@
+package at.sw2016.getgoing.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.robotium.solo.Solo;
+
+import java.util.Date;
+
+import at.sw2016.getgoing.Event;
+import at.sw2016.getgoing.GetGoing;
+
+/**
+ * Created by Johannes on 10.04.2016.
+ */
+public class EventTest extends ActivityInstrumentationTestCase2<GetGoing> {
+    private Solo han;
+
+    private String evtName = "testEvtName";
+    private String evtLoc = "testEvtLoc";
+    private Date evtDate = new Date();
+
+    public EventTest() {
+        super(GetGoing.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        han = new Solo(getInstrumentation(), getActivity());
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testCtor() {
+        Event testEvt = new Event(evtName, evtLoc, evtDate);
+        assertEquals(testEvt.getName(), evtName);
+        assertEquals(testEvt.getLocation(), evtLoc);
+        assertEquals(testEvt.getDate(), evtDate);
+    }
+
+    public void testSetName() {
+        String newEvtName = "some_new_name";
+        Event testEvt = new Event(evtName, evtLoc, evtDate);
+        testEvt.setName(newEvtName);
+        assertEquals("Testing Event.setName()", testEvt.getName(), newEvtName);
+    }
+
+    public void testSetLocation() {
+        String newEvtLoc = "some_new_location";
+        Event testEvt = new Event(evtName, evtLoc, evtDate);
+        testEvt.setLocation(newEvtLoc);
+        assertEquals("Testing Event.setLocation()", testEvt.getLocation(), newEvtLoc);
+    }
+
+    public void testSetDate() {
+        Date newDate = new Date();
+        Event testEvt = new Event(evtName, evtLoc, evtDate);
+        testEvt.setDate(newDate);
+        assertEquals("Testing Event.setDate()", testEvt.getDate(), newDate);
+    }
+}
